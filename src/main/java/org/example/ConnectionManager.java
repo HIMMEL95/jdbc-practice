3e52fd3f560e4d5aa9be204f255adc4deec69818
Himmel <bbluesky7738@gmail.com>
2023-01-26 16:26:03 +0900

    2021-01-26 16:25 jdbc-practice

--- src/main/java/org/example/ConnectionManager.java
+++ src/main/java/org/example/ConnectionManager.java
@@ -4,11 +4,11 @@ import com.zaxxer.hikari.HikariDataSource;
 
 import javax.sql.DataSource;
 
-public class ConnetionManager {
+public class ConnectionManager {
     public static DataSource getDataSource() {
         HikariDataSource hikariDataSource = new HikariDataSource();
         hikariDataSource.setDriverClassName("org.h2.Driver");
-        hikariDataSource.setJdbcUrl("jdbc:h2:mem//localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1");
+        hikariDataSource.setJdbcUrl("jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1");
         hikariDataSource.setUsername("sa");
         hikariDataSource.setPassword("");
 
