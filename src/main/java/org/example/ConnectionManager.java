a6c8bcfb4975b1842d8805ca92eb77c353970ae6
Himmel <bbluesky7738@gmail.com>
2023-02-05 16:44:12 +0900

    2021-02-05 16:44

--- src/main/java/org/example/ConnectionManager.java
+++ src/main/java/org/example/ConnectionManager.java
@@ -3,14 +3,39 @@ package org.example;
 import com.zaxxer.hikari.HikariDataSource;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConnectionManager {
+
+    private static final String DB_URL = "jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1";
+    private static final String DB_DRIVER = "org.h2.Driver";
+    private static final int MAX_POOL_SIZE = 40;
+
+    private static Connection getConnection() throws SQLException {
+        return getDataSource().getConnection();
+
+//        String url = "jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1";
+//        String id = "sa";
+//        String pw = "";
+//
+//        try {
+//            Class.forName("org.h2.Driver");
+//            return DriverManager.getConnection(url, id, pw);
+//        } catch (Exception e) {
+//            return null;
+//        }
+    }
+
     public static DataSource getDataSource() {
         HikariDataSource hikariDataSource = new HikariDataSource();
-        hikariDataSource.setDriverClassName("org.h2.Driver");
-        hikariDataSource.setJdbcUrl("jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1");
+        hikariDataSource.setDriverClassName(DB_DRIVER);
+        hikariDataSource.setJdbcUrl(DB_URL);
         hikariDataSource.setUsername("sa");
         hikariDataSource.setPassword("");
+        hikariDataSource.setMaximumPoolSize(MAX_POOL_SIZE); // Connection 수 설정
+        hikariDataSource.setMinimumIdle(MAX_POOL_SIZE);
 
         return hikariDataSource;
     }
